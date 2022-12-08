bdab2e29e008f57f104d670328c16283261d0f8c
Dartegnian <root@dartegnian.com>
2022-12-09 04:28:57 +0800

    Shuffle adjectives on profile card

--- src/app/components/profile-card/profile-card.component.ts
+++ src/app/components/profile-card/profile-card.component.ts
@@ -28,6 +28,7 @@ export class ProfileCardComponent implements OnInit {
 			name: "DevOps engineer"
 		}
 	];
+	shuffledArray: any[];
 
 	images: Array<string>;
 	coverImage: string;
@@ -41,6 +42,7 @@ export class ProfileCardComponent implements OnInit {
 		private accent: AccentService,
 		@Inject(PLATFORM_ID) private platformId: Object
 	) {
+		this.shuffledArray = this.shuffleArray(this.adjectives);
 		this.images = this.accent.images;
 		this.coverImage = this.images[this.accent.activeIndex];
 		this.secondCoverImage = this.coverImage;
@@ -68,5 +70,20 @@ export class ProfileCardComponent implements OnInit {
 			this.coverImage = this.images[index];
 			this.isSecondCoverImageActive = false;
 		}
+	}
+	
+	shuffleArray(array: any[]) {
+		// Thank you, StackOverflow
+		// https://stackoverflow.com/questions/60787865/randomize-array-in-angular
+		let m = array.length, t, i;
+
+		while (m) {
+			i = Math.floor(Math.random() * m--);
+			t = array[m];
+			array[m] = array[i];
+			array[i] = t;
+		}
+
+		return array;
 	}
 }
