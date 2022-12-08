ee48ed14188e429c9b6134b3e87f9fd60999aead
Dartegnian <root@dartegnian.com>
2022-12-09 03:16:38 +0800

    Write content for music favorites, use in surface blocks

--- src/app/components/surface-test/surface-test.component.ts
+++ src/app/components/surface-test/surface-test.component.ts
@@ -30,6 +30,78 @@ export class SurfaceTestComponent implements OnInit {
 		"material-inverted-secondary"
 	];
 
+	surfaceContent = [
+		{
+			image: "",
+			icon: "",
+			heading: "\"No Celestial\" by Le Sserafim",
+			subHeading: "Top track this month"
+		},
+
+		{
+			image: "",
+			icon: "",
+			heading: "Vocaloid",
+			subHeading: "Favorite genre at the moment"
+		},
+
+		{
+			image: "",
+			icon: "",
+			heading: "Girls' Generation",
+			subHeading: "Favorite girl group"
+		},
+
+		{
+			image: "",
+			icon: "",
+			heading: "Owl City",
+			subHeading: "Favorite single artist"
+		},
+
+		{
+			image: "",
+			icon: "",
+			heading: "\"Girls & Peace\" by Girls' Generation",
+			subHeading: "Favorite album"
+		},
+
+		{
+			image: "",
+			icon: "",
+			heading: "Spotify",
+			subHeading: "Streaming platform"
+		},
+
+		{
+			image: "",
+			icon: "",
+			heading: "Paramore",
+			subHeading: "Favorite band"
+		},
+
+		{
+			image: "",
+			icon: "",
+			heading: "Luka Megurine",
+			subHeading: "Favorite Vocaloid"
+		},
+
+		{
+			image: "",
+			icon: "",
+			heading: "IA/02 -COLOR-",
+			subHeading: "Favorite Vocaloid album"
+		},
+
+		{
+			image: "",
+			icon: "",
+			heading: "130+",
+			subHeading: "Preferred BPM"
+		},
+	];
+
 	constructor() { }
 
 	ngOnInit(): void {
