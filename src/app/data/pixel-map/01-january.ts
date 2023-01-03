87318d1e6a5113db942a9609b557ffa268bedbbf
Dartegnian Velarde <root@dartegnian.com>
2023-01-03 23:39:15 +0800

    Update 01-january.ts

--- src/app/data/pixel-map/01-january.ts
+++ src/app/data/pixel-map/01-january.ts
@@ -3,6 +3,7 @@ import IPixelMapDay from "@interfaces/pixel-map-day.interface";
 const januaryMap: Array<IPixelMapDay> = [
 	{ status: "excellent" },
 	{ status: "above-average" },
+  { status: "bad" },
 ];
 
 export default januaryMap;
