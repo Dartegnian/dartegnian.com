f7fb9b5535ca570444537850d873228943fccae7
Dartegnian Velarde <root@dartegnian.com>
2023-01-05 00:03:14 +0800

    Update 01-january.ts

--- src/app/data/pixel-map/01-january.ts
+++ src/app/data/pixel-map/01-january.ts
@@ -1,7 +1,7 @@
 import IPixelMapDay from "@interfaces/pixel-map-day.interface";
 
 const januaryMap: Array<IPixelMapDay> = [
-	{ status: "excellent" },
+	{ status: "excellent", title: "New Year's Day", reason: "Spent the early morning with someone. Prepared a gift idea. Claimed Starbucks cold cup from sticker set." },
 	{ status: "above-average" },
   { status: "bad" },
   { status: "average" },
