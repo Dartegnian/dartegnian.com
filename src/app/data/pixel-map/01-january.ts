3558a2f6a66f9ffc2c95d88d7a0d5bfba52ab1e1
Dartegnian Velarde <root@dartegnian.com>
2023-01-10 16:24:07 +0800

    Update 01-january.ts

--- src/app/data/pixel-map/01-january.ts
+++ src/app/data/pixel-map/01-january.ts
@@ -9,6 +9,7 @@ const januaryMap: Array<IPixelMapDay> = [
   { status: "bad" },
   { status: "below-average" },
   { status: "excellent", title: "First Friends Outing Day", reason: "Gifted someone our anniversary gift. Watched movies with 1 group of friends, spent the evening with another group of friends. Said goodbye to my best friend and met their family again." },
+  { status: "above-average" },
 ];
 
 export default januaryMap;
