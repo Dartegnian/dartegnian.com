7ec4d135e23d2bc9bb220c811dfa0f3fb91f8762
Dartegnian <root@dartegnian.com>
2023-01-11 19:49:53 +0800

    Make close icon bold, expand mood calendar content

--- src/app/data/pixel-map/01-january.ts
+++ src/app/data/pixel-map/01-january.ts
@@ -1,16 +1,16 @@
 import IPixelMapDay from "@interfaces/pixel-map-day.interface";
 
 const januaryMap: Array<IPixelMapDay> = [
-	{ status: "excellent", title: "New Year's Day", reason: "Spent the early morning with someone. Prepared a gift idea. Claimed Starbucks cold cup from sticker set." },
-	{ status: "above-average" },
-  { status: "bad" },
+  { status: "excellent", icon: "flare", title: "New Year's Day", reason: "Spent the early morning with someone. Prepared a gift idea. Had some time alone in a park to think about things and last year. Claimed Starbucks cold cup from sticker set." },
+  { status: "above-average" },
+  { status: "bad", icon: "sports_esports", reason: "Had a fight with a close, special person. They had a bad day, too, so that kind of snowballed onto me. We were okay within the same night, though." },
   { status: "average" },
   { status: "good" },
-  { status: "bad" },
-  { status: "below-average" },
-  { status: "excellent", title: "First Friends Outing Day", reason: "Gifted someone our anniversary gift. Watched movies with 1 group of friends, spent the evening with another group of friends. Said goodbye to my best friend and met their family again." },
-  { status: "above-average" },
-  { status: "excellent", title: "Anniversary", reason: "My 1st anniversary with someone! We had shared food the entire day: same breakfast and dinner. I opened up a lot of things with them and we looked back at our past." },
+  { status: "bad", reason: "I went out with friends and then I got scolded by somebody. I wasn't there when they needed me. It felt like things were going to reset (once again) and I just wiped my entire Discord profile. I'm so scared of that \"1 mistake that changes everything\" from last year. Since I was with friends, I drank a lot with them." },
+  { status: "below-average", reason: "Met up with friends at BGC and had some ramen. This would be my second-to-the-last time I would see my best friend before he leaves for the military. I was still in pain/shock from the day prior." },
+  { status: "excellent", icon: "movie", title: "First Friends Outing Day", reason: "Gifted someone our anniversary gift! And had some fun alone time, teasing each other and making jokes. Watched movies with 1 group of friends, spent the evening with another group of friends. Said goodbye to my best friend and met their family again after years." },
+  { status: "above-average", icon: "call", reason: "Spent the entire day on call with someone. It was cute, it's been a while since we've had a full day on call and just with each other, mostly." },
+  { status: "excellent", icon: "heart_plus", title: "Anniversary", reason: "My 1st anniversary with someone! We had shared food the entire day: same breakfast and dinner. I opened up a lot of things with them and we looked back at our past." },
 ];
 
 export default januaryMap;
